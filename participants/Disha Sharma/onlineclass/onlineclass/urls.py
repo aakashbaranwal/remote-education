63d26f78ba5adbc1535f7ce7ec31b96842aacad8
Disha Sharma <sharma.disha234@gmail.com>
2020-05-27 00:04:57 +0530

    changes

--- participants/Disha Sharma/onlineclass/onlineclass/urls.py
+++ participants/Disha Sharma/onlineclass/onlineclass/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views 
+from user import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('onlineclass/',)
+    path('profile/',user_views.profile,name="profile"),
+    path('register/',user_views.register,name="register"),
+    path('login/',auth_views.LoginView.as_view(template_name='user/login.html'),name="login"),
+    path('logout/',auth_views.LogoutView.as_view(template_name='user/logout.html'),name="logout")
+
 ]
